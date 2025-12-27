83c1c172f3c5a83fa1bda354983bddbd9d577edd
Nguyen Tan Nhat Huy <jwyy2005@gmail.com>
2025-12-27 16:05:30 +0700

    update

--- Backend/src/configs/cors.js
+++ Backend/src/configs/cors.js
@@ -2,11 +2,17 @@ import { env } from './environment'
 
 export const corsOptions = {
   origin: function (origin, callback) {
+    // Allow multiple origins for development
     const whitelist = [
       env.WEBSITE_DOMAIN_DEV,
-      env.WEBSITE_DOMAIN_PROD
-    ]
+      env.WEBSITE_DOMAIN_PROD,
+      'http://localhost',
+      'http://localhost:80',
+      'http://localhost:3000',
+      'http://localhost:8017'
+    ].filter(Boolean) // Remove undefined values
     
+    // Allow requests with no origin (mobile apps, Postman, etc.)
     if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
